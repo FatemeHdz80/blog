4904cebc933680a7fd8e8ff1a10ddba1c920eb15
Fateme <f.hz7923@gmail.com>
2025-04-12 16:22:24 +0330

    Feed

---
+++ blog/feeds.py
@@ -0,0 +1,19 @@
+from django.contrib.syndication.views import Feed
+from django.urls import reverse_lazy
+from .models import Post
+from django.template.defaultfilters import truncatewords, safe
+
+class LatestPostFeed(Feed):
+    title = 'My Blog'
+    link = reverse_lazy('blog:post_list')
+    description = "This is my post description"
+
+    def items(self):
+        return Post.published.all()[:5]
+    
+    def item_title(self, item):
+        return item.title
+    
+    def item_description(self, item):
+        return safe(truncatewords(item.body, 30))
+    
