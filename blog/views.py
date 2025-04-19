17d66ebd8525db30de2f0c6d90aa58442ecc790b
Fateme <f.hz7923@gmail.com>
2025-04-19 18:59:22 +0330

    Add ...

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,21 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post, Comment
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
-from .forms import EmailPostForm, CommentForm, SearchForm
+from .forms import EmailPostForm, CommentForm, SearchForm, ContactForm
 from django.core.mail import send_mail
 from django.http import HttpResponseRedirect
 from django.contrib import messages
 from taggit.models import Tag
 from django.db.models import Count
 from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank, TrigramSimilarity
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from .forms import SignupForm
+from django.contrib.auth.decorators import login_required
+
 
 
 def post_list(request, tag_slug=None):
@@ -18,22 +25,18 @@ def post_list(request, tag_slug=None):
         tag = get_object_or_404(Tag, slug=tag_slug)
         posts = posts.filter(tags__in=[tag])
 
-    paginator = Paginator(posts, 8)
+    paginator = Paginator(posts, 6)
     page = request.GET.get('page')
     try:
         posts = paginator.page(page)
     except PageNotAnInteger:
         posts = paginator.page(1)
     except EmptyPage:
-        post = paginator.page(paginator.num_pages)
-    context = {"posts": posts, 'page': page, "tag": tag}
+        posts = paginator.page(paginator.num_pages)
+        
+    context = {"posts": posts, 'page': page, "tag": tag, "page_obj": posts}
     return render(request, 'blog/post/list.html', context)
 
-# class PostListView(ListView):
-#     queryset = Post.published.all()
-#     context_object_name = "posts"
-#     paginate_by = 4
-#     template_name = 'blog/post/list.html'
 
 
 def post_detail(request, year, month, day, post):
@@ -86,9 +89,11 @@ def post_share(request, post_id):
                 subject,message,"f.hz7923@gmail.com",
                 [cd['to']]
             )
+            return redirect('blog:post_share', post_id=post.id)
     else:
         form = EmailPostForm()
     return render(request, 'blog/post/share.html', {'post': post, 'form': form})
+
 
 def comment_share(request, year, month, day, post):
     post = get_object_or_404(
@@ -99,17 +104,29 @@ def comment_share(request, year, month, day, post):
         publish__month=month, 
         publish__day=day
     )
+
+
+    if not request.user.is_authenticated:
+        messages.warning(request, "You need to log in to leave a comment.")
+        return HttpResponseRedirect(request.path_info)
+
+    # ✅ اگه لاگین کرده بود:
     if request.method == 'POST':
         comment_form = CommentForm(data=request.POST)
         if comment_form.is_valid():
             new_comment = comment_form.save(commit=False)
             new_comment.post = post
+            new_comment.user = request.user 
             new_comment.save()
+            messages.success(request, "Your comment has been posted.")
             return HttpResponseRedirect(request.path_info)
     else:
         comment_form = CommentForm()
-        return render(request, 'blog/post/detail.html', {"post":post, 'form':comment_form})
 
+    return render(request, 'blog/post/detail.html', {
+        "post": post,
+        'form': comment_form
+    })
 # search view
 def post_search(request):
     form = SearchForm()
@@ -123,5 +140,82 @@ def post_search(request):
             
             search_vector = SearchVector('title', weight='A') + SearchVector('body', weight='C')
             search_query = SearchQuery(query)
-            results = Post.published.annotate(similar = TrigramSimilarity('title', query)).filter(similar__gt=0.2).order_by('-similar')
+            results = Post.published.annotate(similar = TrigramSimilarity('title', query)).filter(similar__gt=0.1).order_by('-similar')
     return render(request, 'blog/post/search.html', {'form':form, 'query':query, 'results':results})
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            next_url = request.POST.get("next")
+            messages.success(request, "Login successful.")
+            return redirect(next_url or "blog:post_list")
+        else:
+            messages.success(request, "❌ There was a problem logging in. Please try again.")
+            return redirect("blog:login_user")
+    else:   
+        return render(request, 'blog/post/login.html')
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "You have successfully logged out!")
+    return redirect("blog:post_list")
+
+def signup_user(request):
+    form = SignupForm()
+
+    if request.method == "POST":
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password1 = form.cleaned_data['password1']
+            user = authenticate(request, username=username, password=password1)
+            login(request, user)
+
+            messages.success(request, "Welcome! Your account has been successfully created.")
+            return redirect("blog:post_list")
+        else:
+            messages.success(request, "An error occurred during your registration!")
+            return redirect("blog:signup_user")
+    else:
+
+        return render(request, 'blog/post/signup.html', {'form': form})
+
+def about(request):
+    return render(request, 'blog/post/about.html')
+
+def contact_view(request):
+    form = ContactForm()
+    
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        
+        if form.is_valid():
+        
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            message = form.cleaned_data['message']
+
+            subject = f"Message from {name}"
+            from_email = email
+            to_email = "f.hz7923@gmail.com" 
+
+            send_mail(
+                subject,
+                message,
+                from_email,
+                [to_email],
+                fail_silently=False,
+            )
+
+            messages.success(request, "Your message was sent successfully!")
+            return redirect('blog:contact')
+        else:
+            form = ContactForm()  
+
+    return render(request, 'blog/post/contact.html', {'form': form})
--- my_blog/urls.py
+++ my_blog/urls.py
@@ -18,6 +18,9 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from django.contrib.sitemaps.views import sitemap
 from blog.sitemaps import PostSitemap
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 
 sitemaps = {
@@ -26,6 +29,12 @@ sitemaps = {
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', include('blog.urls')),
-    re_path(r'^sitemap\.xml/?$', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap')
-]
+    path('', include('blog.urls')),
+    re_path(r'^sitemap\.xml/?$', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
+    path('reset-password/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
