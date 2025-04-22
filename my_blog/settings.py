b9bbbada873ca80c5680cf288d9521b430f96e2b
Fateme <f.hz7923@gmail.com>
2025-04-23 01:50:30 +0330

    first commit for Render deploy

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -11,8 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-from my_blog.local_settings import *
+from decouple import config
+import dj_database_url
 import os
+
 # from .email_info import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -24,7 +26,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="my-blog.onrender.com", cast=lambda v: v.split(","))
+
+
+
+
 
 
 # Application definition
@@ -117,7 +123,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_URLS = ['static/']
 STATICFILES_DIRS =[
     BASE_DIR / "static"
 ]
@@ -132,21 +137,36 @@ MEDIA_ROOT = BASE_DIR / 'media'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 DEBUG = False
-ALLOWED_HOSTS = ['.liara.run']
 CSRF_TRUSTED_ORIGINS = ['https://*.liara.run']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'fallback-secret-key')
-DEBUG = os.getenv('DJANGO_DEBUG', 'False') == 'True'
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-ch3+lmv5ekpr$!&ytai%_!%yoz4pn_fiz9j&j$j4eh4-i^rsxe'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = config('DEBUG', default=False, cast=bool)
+
+# Email settings 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com' 
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'f.hz7923@gmail.com'
+EMAIL_HOST_PASSWORD = 'ijig ytjw bvcd etnt'
+
+
+
+# Reaet Password
+DEFAULT_FROM_EMAIL = 'noreply@example.com'
+
+# Database
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST', 'localhost'),
-        'PORT': os.getenv('DB_PORT', '5432'),
-    }
+    'default': dj_database_url.config(
+        default='postgres://user:password@localhost/dbname',
+        conn_max_age=600
+    )
 }
+
